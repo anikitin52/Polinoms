8cbe25eef246ff107cd8c0ff04032e09d5b4e833
a.nikitin52 <a.nikitin0552@gmail.com>
2025-04-24 21:09:58 +0300

    Изменена структура кода в классе хеш таблицы

--- include/HashTableC.h
+++ include/HashTableC.h
@@ -17,29 +17,7 @@ class HashTable {
 	size_t stringToNumber(const std::string& key) const;
 	size_t hash(string key);
 
-	void resize() {
-		size_t newCapacity = capacity * 2;
-		vector<list<pair<string, Polinom>>> newData(newCapacity);
-		
-		std::random_device rd;
-		std::mt19937 gen(rd());
-		std::uniform_int_distribution<size_t> dist(1, p - 1);
-
-		a = dist(gen);
-		b = dist(gen);
-		
-		capacity = newCapacity;
-		
-		for (auto& chain : data) {
-			for (auto& list : chain) {
-				size_t newIndex = hash(list.first);
-				newData[newIndex].push_back(list);
-			}
-		}
-		data = std::move(newData);
-		capacity = newCapacity;
-		
-	}
+	void resize();
 
 public:
 	HashTable(int size);
--- src/HashTableC.cpp
+++ src/HashTableC.cpp
@@ -16,6 +16,31 @@ size_t HashTable::hash(string key)
     return ((a * x + b) % p) % capacity;
 }
 
+void HashTable::resize()
+{
+    size_t newCapacity = capacity * 2;
+    vector<list<pair<string, Polinom>>> newData(newCapacity);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<size_t> dist(1, p - 1);
+
+    a = dist(gen);
+    b = dist(gen);
+
+    capacity = newCapacity;
+
+    for (auto& chain : data) {
+        for (auto& list : chain) {
+            size_t newIndex = hash(list.first);
+            newData[newIndex].push_back(list);
+        }
+    }
+    data = std::move(newData);
+    capacity = newCapacity;
+
+}
+
 
 HashTable::HashTable(int size = 10): capacity(size), data(size)
 {
