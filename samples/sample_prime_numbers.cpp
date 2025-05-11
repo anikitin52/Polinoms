bd22820ef3d6418e33475617eb3fd9231c6a71c7
DariyaSavva <savvadariya@yandex.ru>
2025-05-11 15:19:40 +0300

    modifiziren database

--- samples/sample_prime_numbers.cpp
+++ samples/sample_prime_numbers.cpp
@@ -10,6 +10,86 @@
 #include <fstream>
 #include <string>
 
+class DataBase {
+    Table database1;
+    SortedTable database2;
+    AVLtree database3;
+    RedBlackTree database4;
+    HashTable database5 = HashTable(100);
+    DirectHashTable<100> database6;
+    int marker;
+public:
+    DataBase(int _marker) : marker(_marker) {}
+    void Insert(string key, Polinom p) {
+        switch (marker) {
+        case 1: {
+            database1.Insert(key, p);
+        }
+        case 2: {
+            database2.Insert(key, p);
+        }
+        case 3: {
+            database3.Insert(key, p);
+        }
+        case 4: {
+            database4.Insert(key, p);
+        }
+        case 5: {
+            database5.Insert(key, p);
+        }
+        case 6: {
+            database6.Insert(key, p);
+        }
+        }
+    }
+
+    Polinom* Find(string key) {
+        switch (marker) {
+        case 1: {
+            return database1.Find(key);
+        }
+        case 2: {
+            return database2.Find(key);
+        }
+        case 3: {
+            return database3.Find(key);
+        }
+        case 4: {
+            return database4.Find(key);
+        }
+        case 5: {
+            //return database5.Find(key);
+        }
+        case 6: {
+            return database6.Find(key);
+        }
+        }
+    }
+
+
+    void Delete(string key) {
+        switch (marker) {
+        case 1: {
+            database1.Delete(key);
+        }
+        case 2: {
+            database2.Delete(key);
+        }
+        case 3: {
+            database3.Delete(key);
+        }
+        case 4: {
+            database4.Delete(key);
+        }
+        case 5: {
+            database5.Delete(key);
+        }
+        case 6: {
+            database6.Delete(key);
+        }
+        }
+    }
+};
 
 int main()
 {
@@ -19,22 +99,19 @@ int main()
     cout << "Доступные структуры хранения: 1 - неупорядоченная таблица, 2 - упорядоченная таблица" << endl;
     cout << "Доступные структуры хранения: 3 - AVL-дерево, 4 - красно-чёрное дерево" << endl;
     cout << "Доступные структуры хранения: 5 - хэш-таблица методом цепочек, 6 - хэш-таблица методом открытой адресации" << endl;
-    RedBlackTree database;  // по умолчанию красно-чёрное дерево
     std::string input;
+
     while (true) {
         std::getline(std::cin, input);
         if (input == "1") {
-            Table database;
             std::cout << "Вы выбрали неупорядоченную таблицу." << std::endl;
             break;
         }
         if (input == "2") {
-            RedBlackTree database;
             std::cout << "Вы выбрали неупорядоченную таблицу." << std::endl;
             break;
         }
         if (input == "3") {
-            AVLtree database;
             std::cout << "Вы выбрали AVL-дерево." << std::endl;
             break;
         }
@@ -43,13 +120,10 @@ int main()
             break;
         }
         if (input == "5") {
-            HashTable database(100);
-            // HashTable database;
             std::cout << "Вы выбрали хэш-таблицу методом цепочек." << std::endl;
             break;
         }
         if (input == "6") {
-            DirectHashTable<100> database;
             std::cout << "Вы выбрали хэш-таблицу методом открытой адресации" << std::endl;
             break;
         }
@@ -58,6 +132,9 @@ int main()
         cout << "Доступные структуры хранения: 3 - AVL-дерево, 4 - красно-чёрное дерево" << endl;
         cout << "Доступные структуры хранения: 5 - хэш-таблица методом цепочек, 6 - хэш-таблица методом открытой адресации" << endl;
     }
+
+    DataBase database(stoi(input));
+
 
 	std::string line;
 	std::ifstream in("TextFile1.txt");
@@ -76,6 +153,7 @@ int main()
         }
     }
     in.close();
+
 
     while (true) {
         cout << "Введите тип совершаемой операции: 1 - вставка, 2 - удаление, 3 - поиск" << endl;
@@ -109,9 +187,11 @@ int main()
             }
             else {
                 std::cout << "Элемент с таким ключом найден. " << std::endl;
-                std::cout << "Возвращаемое значение: " << *p;
+                std::cout << "Возвращаемое значение: " << *p << endl;
             }
         }
     }
     return 0;
 }
+
+
